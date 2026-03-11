347f1fda4e5b433b5060f0bc9c7845d4391ee4de
wlstp8473 <65928720+wlstp8473@users.noreply.github.com>
2026-03-11 17:52:35 +0900

    접근 방식 주석 추가.java
    문제 풀이에 대한 접근 방식을 주석문으로 추가하였음

--- 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.java
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.java
@@ -1,3 +1,10 @@
+/*
+"숫자 연산자 숫자 연산자 숫자" 구조
+수식 계산은 항상 첫 숫자부터 시작 => answer에 첫 숫자 저장 후
+연산자만 반복문으로 처리 (i += 2, 만약 i++하게 되면 불필요한 반복 발생)
+
+*/
+
 class Solution {
     public int solution(String my_string) {
         String[] arr = my_string.split(" ");
