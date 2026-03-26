31775aeed130e865477a21b62fd563de3f545c1b
wlstp8473 <65928720+wlstp8473@users.noreply.github.com>
2026-03-26 14:52:57 +0900

    [level 0] Title: 공백으로 구분하기 1, Time: 0.23 ms, Memory: 85.3 MB -BaekjoonHub

--- 프로그래머스/0/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
+++ 프로그래머스/0/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
@@ -4,3 +4,28 @@ class Solution {
         return my_string.split(" ");
     }
 }
+
+//직접 구현
+/*
+import java.util.*;
+
+class Solution {
+    public String[] solution(String my_string) {
+        List<String> list = new ArrayList<>();
+        String word = "";
+        
+        for (int i = 0; i < my_string.length(); i++) {
+            char c = my_string.charAt(i);
+            
+            if (c == ' ') {
+                list.add(word);
+                word = "";
+            } else {
+                word += c;
+            }
+        }
+        list.add(word); 
+        return list.toArray(new String[0]);
+    }
+}
+*/
