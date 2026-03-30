c3fd7b56b196ab90e60e302f1b2ca6aa73162097
wlstp8473 <65928720+wlstp8473@users.noreply.github.com>
2026-03-30 16:27:06 +0900

    Update 배열에서 문자열 대소문자 변환하기.java
    풀이방법 추가

--- 프로그래머스/0/181875. 배열에서 문자열 대소문자 변환하기/배열에서 문자열 대소문자 변환하기.java
+++ 프로그래머스/0/181875. 배열에서 문자열 대소문자 변환하기/배열에서 문자열 대소문자 변환하기.java
@@ -13,3 +13,17 @@ class Solution {
         return answer;
     }
 }
+/*
+//배열 생성 없이 풀어보기
+
+class Solution {
+    public String[] solution(String[] strArr) {
+        for (int i = 0; i < strArr.length; i++) {
+            strArr[i] = (i % 2 == 0)
+                    ? strArr[i].toLowerCase()
+                    : strArr[i].toUpperCase();
+        }
+        return strArr;
+    }
+}
+*/
