ee52fc31d46395de76953b79df7c7ca8c4cfecb7
wlstp8473 <65928720+wlstp8473@users.noreply.github.com>
2026-03-30 17:04:27 +0900

    [level 0] Title: 할 일 목록, Time: 0.05 ms, Memory: 84.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,17 @@
+// finished[i] == false인 것만 담아서 반환
+
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+        List<String> result = new ArrayList<>();
+        
+        for(int i = 0; i < todo_list.length; i++)
+        {
+            if(!finished[i]){
+                result.add(todo_list[i]);
+            }
+        }
+        return result.toArray(new String[0]);
+    }
+}
