c8036f9cf008b3d8f57923d43355b0cbb242b97b
wlstp8473 <65928720+wlstp8473@users.noreply.github.com>
2026-04-02 16:25:44 +0900

    [level 0] Title: 부분 문자열 이어 붙여 문자열 만들기, Time: 0.10 ms, Memory: 83.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.java
@@ -0,0 +1,18 @@
+/*
+substring(start, end)는 end 직전까지라서,
+문제처럼 e까지 포함하려면 substring(s, e + 1)
+*/
+
+class Solution {
+    public String solution(String[] my_strings, int[][] parts) {
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = 0; i < my_strings.length; i++){
+            int s = parts[i][0];
+            int e = parts[i][1];
+            
+            sb.append(my_strings[i].substring(s, e + 1));
+        }
+        return sb.toString();
+    }
+}
