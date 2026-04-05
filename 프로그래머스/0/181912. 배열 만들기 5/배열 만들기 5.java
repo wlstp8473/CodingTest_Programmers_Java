3b8cecc5ce5033177b11b269d977d30be03adfb7
wlstp8473 <65928720+wlstp8473@users.noreply.github.com>
2026-04-05 16:47:17 +0900

    [level 0] Title: 배열 만들기 5, Time: 7.67 ms, Memory: 106 MB -BaekjoonHub

---
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.java
@@ -0,0 +1,25 @@
+
+/*
+substring(s, s + l) 활용
+*/
+
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] intStrs, int k, int s, int l) {
+        List<Integer> list = new ArrayList<>();
+        
+        for(String str : intStrs){
+            int num = Integer.parseInt(str.substring(s, s + l));
+            
+            if(num > k){
+                list.add(num);
+            }
+        }
+        int[] answer = new int[list.size()];
+        for(int i = 0; i < list.size(); i++){
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+}
