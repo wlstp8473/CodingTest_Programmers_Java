a01a1c3b5d96735e99b7000b4160499a5f419de5
wlstp8473 <65928720+wlstp8473@users.noreply.github.com>
2026-04-06 09:45:56 +0900

    [level 0] Title: 문자열 묶기, Time: 7.12 ms, Memory: 123 MB -BaekjoonHub

---
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
@@ -0,0 +1,39 @@
+//문자의 길이별 빈도수 체크(문자의 길이가 명확 => 배열 사용)
+
+class Solution {
+    public int solution(String[] strArr) {
+        int[] count = new int[31]; //문자열의 최대 길이는 30이니
+        int max = 0;
+        
+        for(String str: strArr){
+            int len = str.length();
+            count[len]++;
+            max = Math.max(max, count[len]);
+        }
+        return max;
+    }
+}
+
+/*
+//문자의 길이 제한이 없을 떄, 일반적인 풀이
+//map.get(len) + 1 // key 없으면 NullPointerException 발생 주의
+//map.getOrDefault(len, 0) 활용
+
+
+import java.util.*;
+
+class Solution {
+    public int solution(String[] strArr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int max = 0;
+        
+        for (String str : strArr) {
+            int len = str.length();
+            map.put(len, map.getOrDefault(len, 0) + 1);//예외처리 후, 값 증가
+            max = Math.max(max, map.get(len));
+        }
+        
+        return max;
+    }
+}
+*/
