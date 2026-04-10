4e9ce3faccbd56fd44b58727058a91b55040df5a
wlstp8473 <65928720+wlstp8473@users.noreply.github.com>
2026-04-10 13:47:06 +0900

    [level 0] Title: 특수문자 출력하기, Time: 138.62 ms, Memory: 67.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -0,0 +1,11 @@
+/*
+백슬래시 (\) 출력: \\
+쌍따옴표 (") 출력: \" (문자열 안에서 출력 시, escape 필요)
+*/
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("!@#$%^&*(\\'\"<>?:;");
+    }
+}
